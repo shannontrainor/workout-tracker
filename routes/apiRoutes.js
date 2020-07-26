e95b429f297428671205054147d81c3e85ec22a2
Shannon Trainor <sltrainor@outlook.com>
2020-07-26 14:30:51 -0400

    added get/push/post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,52 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
+const { db } = require("../models/workout");
 
-router.post("/api/workout", (req, res) => {
-    Workout.create({}).then()
+
+//add to previous
+//add new
+//combined weight
+
+//GET workouts
+router.get("/workouts", (req,res) => {
+    db.Workout.find({}).then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+    });
+    
 });
+
+//add new - POST
+router.post("/api/workout", (req, res) => {
+    Workout.create({}).then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+    });
+});
+
+//add to existing - PUT
+router.put("/api/workout/:id", (req,res) => {
+    Workout.updateOne(
+        { _id: req.params.id },
+        { $push: {exercises: req.body } }
+    )
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+    });
+        
+});
+
 
