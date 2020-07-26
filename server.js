d95f925e017712b55a9d5db5575d31e29cffabc9
Shannon Trainor <sltrainor@outlook.com>
2020-07-26 14:08:21 -0400

    updated to add mongoURI

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 //listening port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //connect to express
 const app = express();
@@ -16,14 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+let MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-//app.use(require('./routes/api.js'));
-app.use(require('./routes/htmlRoutes.js'));
+app.use(require('./routes/api.js'));
+app.use(require('./routes/views.js'));
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
+    console.log(`App listening on port ${PORT}`);
 });
